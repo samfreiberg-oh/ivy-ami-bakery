c35b8bd35cf60a337fa8d23e017b0a2be56046be
Ricardo Rosales <missingcharacter@gmail.com>
2020-12-12 04:47:02 -0600

    download_binaries.sh now has different cases per binary

--- scripts/binaries/download_binaries.sh
+++ scripts/binaries/download_binaries.sh
@@ -26,12 +26,28 @@ function download_if_not_present() {
     if [[ -e "${SYSTEM_BASE_FILES}/${BINARY}" ]]; then
       echo "${BINARY} has been downloaded already, if you want to download a new version"
       echo "delete the current one at ${SYSTEM_BASE_FILES}/${BINARY}"
-      exit 0
+      return 0
     fi
 
     echo "Downloading ${BINARY} using docker (you need to be able to run docker commands)"
     echo "${BINARY} will be at ${SYSTEM_BASE_FILES}/${BINARY}"
-    docker run --rm -v ${SYSTEM_BASE_FILES}:/output "${DOCKER_IMAGE}" /bin/bash -c "cp /usr/bin/bpftrace /output"
+    case ${BINARY} in
+
+      gostatsd)
+        docker run --rm -v ${SYSTEM_BASE_FILES}:/output -e BINARY=${BINARY} \
+            --entrypoint /bin/ash "${DOCKER_IMAGE}" -c 'cp $(which ${BINARY}) /output'
+        ;;
+
+      bpftrace)
+        docker run --rm -v ${SYSTEM_BASE_FILES}:/output -e BINARY=${BINARY} \
+            "${DOCKER_IMAGE}" /bin/bash -c "cp /usr/bin/bpftrace /output"
+        ;;
+
+      *)
+        echo "${BINARY} is not supported"
+        return 1
+        ;;
+    esac
     chmod 0755 "${SYSTEM_BASE_FILES}/${BINARY}"
     echo "Deleting docker image since the same tag might be reused"
     docker rmi "${DOCKER_IMAGE}"
